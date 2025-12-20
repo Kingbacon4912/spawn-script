aca6fae22855cc35d22ddab42f33a1e0428367ea
Goober <ollythemonke@gmail.com>
2025-12-20 13:24:24 -0700

    Add optional thanks message in spawn script
    Added a thanks message option that can be toggled.

--- spawn.cs
+++ spawn.cs
@@ -3,10 +3,18 @@ using UnityEngine;
 public class spawn : MonoBehaviour
 {
     public GameObject prefab;
-
+    public bool ThanksMessage = true
+    
     void Start()
     {
         Instantiate(prefab, transform.position, Quaternion.identity); // will spawn at the object to place this script on
+
+        if(ThanksMessage)
+        {
+            Debug.Log("Hey thanks for using my simple script, you can turn this message off in the script settings. No credits are needed");
+        }
+        
     }
 }
+
 
